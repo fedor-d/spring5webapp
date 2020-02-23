03b2a5c81b8271e17da064faea1f92de25e4bd62
FedorD <fedord.dev@gmail.com>
2020-02-23 11:54:50 +0700

    Add POJOs

---
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -0,0 +1,47 @@
+package guru.springframework.spring5webapp.model;
+
+import java.util.Set;
+
+/**
+ * @author FedorD
+ * @date 2020-02-23
+ */
+public class Book {
+
+    private String title;
+    private String isbn;
+    private Set<Author> authors;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
+    public Book() {
+    }
+
+    public Book(String title, String isbn, Set<Author> authors) {
+        this.title = title;
+        this.isbn = isbn;
+        this.authors = authors;
+    }
+}
